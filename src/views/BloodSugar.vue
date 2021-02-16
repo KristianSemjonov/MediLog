b6b8207bfbf624bae54d088cf0e89ab3268004cf
helena21 <helenaisrael81@gmail.com>
2021-02-16 12:17:26 +0200

    addWeight.vue - bmi calculator still needs review, logos changed in all files

--- src/views/BloodSugar.vue
+++ src/views/BloodSugar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bloodsugar">
-    <img alt="Medilog logo" src="../assets/MedilogSmall.jpg" height="400px">
+    <img alt="Medilog logo" src="../assets/mediloglarge_cut.jpg" height="240px" width="auto">
     <h3>Siin saate sisestada veresuhkru mõõtmistulemusi ja vaadata päevikut oma mõõtmistulemustega</h3>
     <br>
     <router-link to="/veresuhkrum66tmine">Sisesta veresuhkru mõõtmistulemused</router-link> |
--- src/views/DiaryBloodPressure.vue
+++ src/views/DiaryBloodPressure.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="diarybloodpressure">
-    <img alt="Medilog logo" src="../assets/MedilogSmall.jpg" height="300px">
+    <img alt="Medilog logo" src="../assets/mediloglarge_cut.jpg" height="240px" width="auto">
     <h3>Vaadake päevikut vererõhu mõõtmistulemustega</h3>
     <p>Kasutaja ID: <input v-model="userId" placeholder="kustutame, kui login toimib!"></p>
     <button v-on:click="getData()">Vaata päevikut vererõhu mõõtmistulemustega</button>
