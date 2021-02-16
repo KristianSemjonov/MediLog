c56ba78f6a9b396f230f5b5f691092a832812430
MarikaKaldasaun <marika.kaldasaun@gmail.com>
2021-02-16 12:01:19 +0200

    vueTemp

--- src/views/Weight.vue
+++ src/views/Weight.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="weight">
     <img alt="Medilog logo" src="../assets/MedilogSmall.jpg" height="400px">
-    <h3>Siin saate sisestada kehakaaluu mõõtmistulemusi ja vaadata päevikut oma mõõtmistulemustega</h3>
+    <h3>Siin saate sisestada kehakaalu mõõtmistulemusi ja vaadata päevikut oma mõõtmistulemustega</h3>
     <br>
-    <router-link to="/kehakaaluum66tmine">Sisesta kehakaalu mõõtmistulemused</router-link> |
+    <router-link to="/kehakaalum66tmine">Sisesta kehakaalu mõõtmistulemused</router-link> |
     <router-link to="/kehakaalupaevik">Vaata päevikut kehakaalu mõõtmistulemustega</router-link>
   </div>
 </template>
